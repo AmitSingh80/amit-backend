9c5214547a4f36eddfdd58bb0f0e50a8340017ed
AmitSingh80 <heyamitsingh7@gmail.com>
2025-05-10 15:36:40 +0530

    connect mongodb

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+
+const connectDB= async ()=>{
+    try {
+        const conectionString = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`Mongodb connection successfuly: ${conectionString.connection.host}`);
+        
+    } catch (error) {
+        console.log("connection failed :",error);
+        throw error
+        
+        
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,57 @@
+
+
+import mongoose from "mongoose";
+import{ DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './path'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app= express()
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Errr:",error);
+            throw error
+            
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.log("err:",error);
+        throw error
+        
+        
+    }
+}) ()
+*/
