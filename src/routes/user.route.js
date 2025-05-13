585d5941a08c925b9e84560327bcb0117d0c8931
AmitSingh80 <heyamitsingh7@gmail.com>
2025-05-13 21:41:30 +0530

    route update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -35,7 +35,7 @@ router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar
 router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
-Router.route("/history").get(verifyJWT,getWatchHistory)
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router
